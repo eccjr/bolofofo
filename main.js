a04bff564a09556273bba7f2a481f42f56c203cd
paolaguedes <53832972+paolaguedes@users.noreply.github.com>
2021-10-07 18:08:06 -0300

    okok

--- main.js
+++ main.js
@@ -4,6 +4,46 @@ function favoritarCoracao() {
     for(let i in coracoes){
     coracoes[i].onclick = event => event.currentTarget.classList.toggle("fas")  
     }
-  }
+}
   
   favoritarCoracao()
+
+// para adicionar scroll macio
+document.querySelectorAll('a[href^="#"]').forEach(function (element) {
+  if (!element.hash) return
+  if (element.origin + element.pathname !== self.location.href) return
+
+  (function (destination) {
+    element.addEventListener(
+      'click',
+      function (event) {
+        window.scrollTo({
+          top: destination.offsetTop,
+          behavior: 'smooth',
+        })
+        event.preventDefault()
+      },
+      false
+    )
+  })(document.querySelector(element.hash))
+})
+
+// slider 
+
+//   const imgs = document.getElementById("imgs")
+//   const img = document.querySelectorAll("#imgs img")
+
+//   let idx = 0
+
+//   function carrossel(){
+//     idx++
+
+//     if(idx > img.length -1) 
+//         idx=0
+
+//     imgs.style.transform = `translateX(${-idx * 1100}px)`
+
+// } setInterval(carrossel, 1800)
+
+
+
