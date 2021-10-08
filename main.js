e4361578682fe964e6183bdd7c549a984cc52ede
paolaguedes <53832972+paolaguedes@users.noreply.github.com>
2021-10-07 23:51:15 -0300

    menu js

--- main.js
+++ main.js
@@ -45,5 +45,25 @@ document.querySelectorAll('a[href^="#"]').forEach(function (element) {
 
 // } setInterval(carrossel, 1800)
 
+let arrowL = document.getElementsByClassName("fa-chevron-left")[0]
+let arrowR = document.getElementsByClassName("fa-chevron-right")[0]
+let card = document.getElementsByClassName("card1")
+let card2 = document.getElementsByClassName("card2")
+
+arrowR.addEventListener("click", function () {
+  for(let i in card)
+      card[i].classList.toggle("cardOff")
+
+  for(let id in card2) 
+      card2[id].classList.toggle("cardOff")
+})
+
+arrowL.addEventListener("click", function () {
+  for(let i in card)
+      card[i].classList.toggle("cardOff")
+
+  for(let id in card2) 
+      card2[id].classList.toggle("cardOff")
+})
 
 
