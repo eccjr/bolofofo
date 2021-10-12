048975188a6b4c5bcf7b9bf1e6170f39d6d7ba79
paolaguedes <53832972+paolaguedes@users.noreply.github.com>
2021-10-11 22:40:09 -0300

    arrumei o cardapio

--- assets/js/arrows.js
+++ assets/js/arrows.js
@@ -1,23 +1,24 @@
 
 const arrowL = document.getElementsByClassName("fa-chevron-left")[0]
 const arrowR = document.getElementsByClassName("fa-chevron-right")[0]
-let card = document.getElementsByClassName("card1")
-let card2 = document.getElementsByClassName("card2")
+let card = document.querySelectorAll(".card1")
+let card2 = document.querySelectorAll(".card2")
 
-arrowR.addEventListener("click", () => {
-  for(let i in card)
+
+arrowR.addEventListener('click', () => {
+    for(let i=0; i< card.length; i++)
       card[i].classList.toggle("cardOff")
 
-  for(let id in card2) 
-      card2[id].classList.toggle("cardOff")
+    for(let i=0; i< card2.length; i++)
+      card2[i].classList.toggle("cardOff")
 })
 
-arrowL.addEventListener("click", () =>  {
-  for(let i in card)
+arrowL.addEventListener('click', () =>  {
+    for(let i=0; i< card.length; i++)
       card[i].classList.toggle("cardOff")
 
-  for(let id in card2) 
-      card2[id].classList.toggle("cardOff")
+    for(let i=0; i< card2.length; i++)
+      card2[i].classList.toggle("cardOff")
 })
 
 
